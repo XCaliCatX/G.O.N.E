c56fac5cd899687b9229b9849c528f508c5a189f
XCaliCatX <60245827+XCaliCatX@users.noreply.github.com>
2022-05-11 18:32:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ GameOFNim
 """
 
 from games import *
-import pygame
-pygame.init()
+
 
 class GameOfNim(Game):
     # YOUR CODE GOES HERE
@@ -72,17 +71,9 @@ class GameOfNim(Game):
 
 
 if __name__ == "__main__":
-    YELLOW = (255, 255, 0)
-    CYAN = (0, 255, 255)
-    MAGENTA = (255, 0, 255)
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
-    BLUE = (0, 0, 255)
-    BLACK = (0, 0, 0)
-    GRAY = (127, 127, 127)
-    WHITE = (255, 255, 255)
 
-    print(pygame.font.get_fonts())
+
+
     nim = GameOfNim(board=[0, 5, 3, 1])  # Creating the game instance
    # nim = GameOfNim(board=[0,5,3,1,4,6,7,8])
     #nim = GameOfNim(board=[7, 5, 3, 1]) # a much larger tree to search
@@ -90,30 +81,8 @@ if __name__ == "__main__":
     board= nim.initial.board
     print(nim.initial.moves) # must be [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2,1), (2, 2), (2, 3), (3, 1)]
     print(nim.result(nim.initial, (1,3) ))
-    utility = nim.play_game(alpha_beta_player, query_player) # computer moves first
-    if (utility < 0):
-        print("MIN won the game")
-    else:
-       print("MAX won the game")
-
-    size = [800,600]
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("G.O.N.E")
-    icon = pygame.image.load("space.png")
-    pygame.display.set_icon(icon)
-    done = False
-    clock = pygame.time.Clock()
-    titleFont = pygame.font.Font(None, 32)
-    titleText = titleFont.render("Game of Nim Extension", True, BLACK)
-    while not done:
-
-        clock.tick(10)
-
-        for event in pygame.event.get():
-           if event.type == pygame.QUIT:
-              done=True
-
-
-        screen.fill(WHITE)
-        screen.blit(titleText, (290,15))
-        pygame.display.flip()
+   # utility = nim.play_game(alpha_beta_player, query_player) # computer moves first
+    #if (utility < 0):
+    #    print("MIN won the game")
+    #else:
+     #  print("MAX won the game")
