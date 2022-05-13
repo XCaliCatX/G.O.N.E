5a3f9060ad527b83184f6c05effccc5928397db8
XCaliCatX <60245827+XCaliCatX@users.noreply.github.com>
2022-05-12 20:44:37 -0700

    Update gone.py

--- gone.py
+++ gone.py
@@ -2,7 +2,6 @@ import sys, pygame, random, pygame_gui
 from games import *
 from main import GameOfNim
 
-
 pygame.init()
 size = width, height = 1024, 768
 screen = pygame.display.set_mode(size)
@@ -19,6 +18,8 @@ def draw_text(text, font, color, surface, x, y):
 font = pygame.font.SysFont(None, 60)
 COLOR_ACTIVE = pygame.Color('lightskyblue3')
 COLOR_INACTIVE = pygame.Color('dodgerblue2')
+pygame_icon = pygame.image.load('space.png')
+pygame.display.set_icon(pygame_icon)
 black = (0, 0, 0)
 white = (255, 255, 255)
 YELLOW = (255, 255, 0)
@@ -38,25 +39,30 @@ def main_menu():
     screen.fill("black")
 
     MENU_TEXT = font.render("Game of Nim Extension", True, "#b68f40")
-    MENU_RECT = MENU_TEXT.get_rect(center=(400, 100))
+    MENU_RECT = MENU_TEXT.get_rect(center=(520, 100))
     screen.blit(MENU_TEXT, MENU_RECT)
 
-    input_one_label = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((300, 170), (200, 25)), text="# Rows", manager=manager)
-    input_one = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((300, 190), (200, 50)),manager=manager)
+    input_one_label = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((420, 170), (200, 25)), text="# Rows",
+                                                  manager=manager)
+    input_one = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((420, 190), (200, 50)), manager=manager)
     input_one.set_allowed_characters(allowed_characters="numbers")
     # input_one.set_text_length_limit(limit=1)
 
-    input_two_label = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((300, 245), (200, 25)), text="Max # Sticks", manager=manager)
-    input_two = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((300, 265), (200, 50)),manager=manager)
+    input_two_label = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((420, 245), (200, 25)), text="Max # Sticks",
+                                                  manager=manager)
+    input_two = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((420, 265), (200, 50)), manager=manager)
     input_two.set_allowed_characters(allowed_characters="numbers")
     # input_two.set_text_length_limit(limit=1)
 
-    start_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((300, 325), (200, 50)), text='START GAME', manager=manager)
-    how_to_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((300, 385), (200, 50)), text='HOW TO PLAY', manager=manager)
-    exit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((300, 445), (200, 50)), text='EXIT', manager=manager)
+    start_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((420, 325), (200, 50)), text='START GAME',
+                                                manager=manager)
+    how_to_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((420, 385), (200, 50)), text='HOW TO PLAY',
+                                                 manager=manager)
+    exit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((420, 445), (200, 50)), text='EXIT',
+                                               manager=manager)
 
     while True:
-        time_delta = CLOCK.tick(60)/1000.0
+        time_delta = CLOCK.tick(60) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
@@ -85,18 +91,20 @@ def main_menu():
 
         pygame.display.update()
 
+
 def how_to_play():
     CLOCK = pygame.time.Clock()
 
     screen.fill("black")
-    rules_text = "Here we will explain the rules to the game.  It may not be very long, but there should be plenty of space to work with."
-    
-    
-    rules = pygame_gui.elements.UITextBox(html_text=rules_text, relative_rect=pygame.Rect((50, 50), (700, 300)), manager=manager)
-    back_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((300, 445), (200, 50)), text='BACK', manager=manager)
+    rules_text = "Rules: The goal of the game to to not pick the last fern. You may pick any number of ferns from any given row, but only one row at a time."
+
+    rules = pygame_gui.elements.UITextBox(html_text=rules_text, relative_rect=pygame.Rect((175, 50), (700, 300)),
+                                          manager=manager)
+    back_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((420, 445), (200, 50)), text='BACK',
+                                               manager=manager)
 
     while True:
-        time_delta = CLOCK.tick(60)/1000.0
+        time_delta = CLOCK.tick(60) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
@@ -115,6 +123,7 @@ def how_to_play():
 
         pygame.display.update()
 
+
 click = False
 
 
@@ -123,12 +132,12 @@ def game(amt, user_max):
     stick_list = []
     stickrect_list = []
     b = []
-    stick_y = 0 # space between the rows of sticks
+    stick_y = 0  # space between the rows of sticks
     stick = pygame.image.load('fern.png')
     for i in range(amt):
-        stick_x = 0 # space between the columns of sticks
+        stick_x = 0  # space between the columns of sticks
         rand_range = random.randrange(1, user_max)
-        b.append(rand_range) # for the GameOfNim function
+        b.append(rand_range)  # for the GameOfNim function
         for j in range(rand_range):
             stick_list.append(stick)
             stickrect = stick.get_rect()
@@ -141,7 +150,7 @@ def game(amt, user_max):
     gom = GameOfNim(board=b)
     while 1:
         # mx, my = pygame.mouse.get_pos()
-        for i in range(amt-1):
+        for i in range(amt - 1):
             pygame.draw.rect(screen, (255, 0, 0), stickrect_list[i])
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
@@ -165,7 +174,15 @@ def game(amt, user_max):
         for i in range(amt):
             for j in range(len(stickrect_list)):
                 screen.blit(stick_list[j], stickrect_list[j])
+
+
         pygame.display.flip()
+
+        #get player move
+        #or get AI move
+        #remove from board, end of lists/rows or create new board?
+        #redisplay new board^^
+        #when winner if found display winner and allow to go back to main menu
         mainClock.tick(60)
 
 
