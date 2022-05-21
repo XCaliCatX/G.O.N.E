a405ae50d4018a6028fce56d39af6f1b6e5fa992
Kim Eaton <60245827+XCaliCatX@users.noreply.github.com>
2022-05-20 20:59:26 -0700

    Final Update

--- game_of_nim.py
+++ game_of_nim.py
@@ -51,6 +51,12 @@ class GameOfNim(Game):
                 empty = False
         return empty
 
+    def eval(self,state):
+        if (self.terminal_test(state) == True):
+            return 1
+        else:
+            return 0
+
     def utility(self, state, player):
         if(self.terminal_test(state) == True):
             if (state.to_move=='MAX') :
@@ -62,8 +68,19 @@ class GameOfNim(Game):
         else:
             return 0
      #def to_move(self, state):  # default implementation in game should be enough
-    def eval(self,state):
-        if (self.terminal_test(state) == True):
-            return 1
-        else:
-            return 0
+
+
+if __name__ == "__main__":
+    exit
+    #nim = GameOfNim(board=[0, 5, 3, 1])  # Creating the game instance
+    # nim = GameOfNim(board=[0,5,3,1])
+    nim = GameOfNim(board=[5, 5, 5, 5, 5, 5, 5, 5, 5, 5])
+    #nim = GameOfNim(board=[7, 5, 3, 1]) # a much larger tree to search
+    print(nim.initial.board) # must be [0, 5, 3, 1]
+    print(nim.initial.moves) # must be [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2,1), (2, 2), (2, 3), (3, 1)]
+    print(nim.result(nim.initial, (1,3) ))
+    utility = nim.play_game(alpha_beta_player, query_player) # computer moves first
+    if (utility < 0):
+        print("MIN won the game")
+    else:
+        print("MAX won the game")
